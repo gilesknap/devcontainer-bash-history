2c3b59a0995eb71ed4dad9b5b3a99c1bc1e2190a
giles knap <gilesknap@gmail.com>
2024-11-26 16:37:08 +0000

    extract config files from install.sh

--- .devcontainer/features/bash-config/install.sh
+++ .devcontainer/features/bash-config/install.sh
@@ -1,71 +1,25 @@
 #!/bin/bash
+
+# abort on error
 set -e
+
+# discover where the install.sh and peers have been extracted to
+this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 
 echo "Activating feature 'terminal-history'"
 echo "User: ${_REMOTE_USER}     User home: ${_REMOTE_USER_HOME}"
 
-mkdir -p $CONFIG_STAGING
+# This script is run at container build time.
+#
+# Here we place all the configuration files in a staging area.
+#
+# Then at container runtime, when the hosts' config folder is mounted in, we
+# can copy from the staging area to the mounted host folder
+#
+# The runtime copy to host folder is performed once oby onCreateCommand.sh
 
-# -------------------------------------------------------------------------------
-cat > $CONFIG_STAGING/onCreateCommand.sh  \
-<< EOF
-#!/bin/bash
+mkdir -p "$CONFIG_STAGING"
+cp -r "$this_dir"/* "$CONFIG_STAGING"
+echo $this_dir > "$CONFIG_STAGING/this_dir"
 
-# copy in the opinionated default settings from the feature
-cp $CONFIG_STAGING/feature_settings_rc $CONFIG_FOLDER/feature_settings_rc
-
-# copy in the user editable settings unless they already exist
-if [[ ! -f $CONFIG_FOLDER/bashrc ]] ; then
-    cp $CONFIG_STAGING/bashrc $CONFIG_FOLDER    
-    cp $CONFIG_STAGING/inputrc $CONFIG_FOLDER
-fi
-
-# hook in the config to the root account
-ln -s $CONFIG_FOLDER/inputrc /root/.inputrc
-echo "source $CONFIG_FOLDER/bashrc" >> /root/.bashrc
-EOF
-
-# -------------------------------------------------------------------------------
-cat > $CONFIG_STAGING/inputrc  \
-<< EOF
-# Readline configuration for bash shell.
-
-# Incremental history searching with up and down arrows (C-P and C-N for old
-# style navigation).
-"\e[A": history-search-backward
-"\e[B": history-search-forward
-
-# Control left and right for word movement
-"\e[5C": forward-word
-"\e[5D": backward-word
-EOF
-
-# -------------------------------------------------------------------------------
-cat > $CONFIG_STAGING/bashrc  \
-<< EOF
-#!/bin/bash
-
-# execute default opinionated settings - delete this line to remove defaults
-source $CONFIG_FOLDER/feature_settings_rc
-
-# add your personal custom settings below
-EOF
-
-# -------------------------------------------------------------------------------
-cat > $CONFIG_STAGING/feature_settings_rc  \
-<< EOF
-#!/bin/bash
-
-# default opinioned bash configuration
-
-# set the prompt
-export PS1="\[\033[1;34m\]\W \[\033[0m\]# "
-
-# enable enternal shared history
-export HISTCONTROL=ignoreboth:erasedups
-export HISTSIZE=-1
-export HISTFILESIZE=-1
-export SAVEHIST=-1
-export HISTFILE=$CONFIG_FOLDER/.bash_eternal_history
-PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
-EOF
---
+++ .devcontainer/features/bash-config/onCreateCommand.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# this script is run once inside the developer container at creation time
+
+# copy in the opinionated default settings from the feature
+cp $CONFIG_STAGING/feature_settings_rc $CONFIG_FOLDER/feature_settings_rc
+
+# copy in the user editable settings unless they already exist
+if [[ ! -f $CONFIG_FOLDER/bashrc ]] ; then
+    cp $CONFIG_STAGING/bashrc $CONFIG_FOLDER
+    cp $CONFIG_STAGING/inputrc $CONFIG_FOLDER
+fi
+
+# hook in the config to the root account
+ln -s $CONFIG_FOLDER/inputrc /root/.inputrc
+echo "source $CONFIG_FOLDER/bashrc" >> /root/.bashrc
